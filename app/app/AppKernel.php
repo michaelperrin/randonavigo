78e504a3dd13068c3fc96b579e2e7654b4af9c81
Michaël Perrin <contact@michaelperrin.fr>
2017-05-19 16:25:10 +0200

    Markdown support for hike description

--- app/app/AppKernel.php
+++ app/app/AppKernel.php
@@ -8,6 +8,7 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
