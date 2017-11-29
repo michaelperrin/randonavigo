20248dac354afb4439f38be888f9529639397eb8
Michaël Perrin <contact@michaelperrin.fr>
2017-11-29 19:32:36 +0100

    Symfony 4 - Add debug and profiler packages

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
