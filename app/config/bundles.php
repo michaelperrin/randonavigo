b48e2e500456c7302cf4cd7e80f9eb9d7e6133e8
Michaël Perrin <contact@michaelperrin.fr>
2017-11-30 12:00:51 +0100

    Symfony 4 update - Add KnpMarkdownBundle back

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
 ];
