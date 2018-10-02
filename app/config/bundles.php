b1aab638ce762f6ec745887771d621d672deaee1
Michaël Perrin <contact@michaelperrin.fr>
2018-10-02 15:24:05 -0500

    Remove unnecessary sensio/framework-extra-bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -3,7 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
--- app/src/Controller/HikeController.php
+++ app/src/Controller/HikeController.php
@@ -4,7 +4,7 @@ namespace RandoNavigo\Controller;
 
 use RandoNavigo\Document\Hike;
 use RandoNavigo\Manager\HikeManager;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
