b352ea268b5ae0996295a8622c85f5c1196466df
Michaël Perrin <contact@michaelperrin.fr>
2020-03-23 07:48:33 -0500

    Use Leaflet GPX from npm

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -24,7 +24,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    .addEntry('hikeShow', './assets/js/hike/show.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
