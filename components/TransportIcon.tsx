698e68c376c1f6b6051d233cd991d1fdf296bafc
Michaël Perrin <michael.perrin84@gmail.com>
2021-10-23 15:07:26 +0200

    Use new transport line icons & add support for light rail lines

--- components/TransportIcon.tsx
+++ components/TransportIcon.tsx
@@ -2,10 +2,13 @@ import Image from 'next/image'
 // import styles from './TransportIcon.module.css' // TODO
 
 const RER_LINES = ['A', 'B', 'C', 'D', 'E']
+const TRAM_LINES = ['T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11', 'T12', 'T13']
 const TRANSILIEN_LINES = ['H', 'J', 'K', 'L', 'N', 'P', 'R', 'U']
+
 
 const isRER = (line: string): boolean => RER_LINES.includes(line)
 const isTransilien = (line: string): boolean => TRANSILIEN_LINES.includes(line)
+const isTram = (line: string): boolean => TRAM_LINES.includes(line)
 
 type TransportIconProps = {
   line: string,
@@ -28,6 +31,15 @@ const TransportIcon = ({ line }: TransportIconProps) => (
         width={24}
         height={24}
         alt="Transilien"
+      />
+    )}
+
+    {isTram(line) && (
+      <Image
+        src="/images/transport/tram.svg"
+        width={24}
+        height={24}
+        alt="Tram"
       />
     )}
 
