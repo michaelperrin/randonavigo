087dafad5d511ba0bc5c1d46c02d5315794252df
Michaël Perrin <michael.perrin84@gmail.com>
2021-10-25 20:54:28 +0200

    Add picture gallery

--- components/hike/Gallery.tsx
+++ components/hike/Gallery.tsx
@@ -1,26 +1,64 @@
 import Image from 'next/image'
+import { useState } from 'react'
+import Lightbox from 'react-image-lightbox'
 import getHikePicturePath from '../../lib/getHikePicturePath'
 import { Hike } from '../../lib/types'
+import 'react-image-lightbox/style.css';
 import styles from './Gallery.module.css'
 
 type GalleryProps = {
   hike: Hike,
 }
 
-const Gallery = ({ hike }: GalleryProps) => (
-  <div className={styles.gallery}>
-    {hike.pictures.map(picture => (
-      <div key={picture} className={styles.thumbnailContainer} style={{ position: 'relative', margin: '1px' }}>
-        <Image
-          className={styles.thumbnail}
-          src={getHikePicturePath(hike, picture)}
-          layout="fill"
-          objectFit="cover"
-          alt=""
-        />
+const Gallery = ({ hike }: GalleryProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [photoIndex, setPhotoIndex] = useState(0)
+
+  const pictures = hike.pictures.map((picture: string) => getHikePicturePath(hike, picture))
+
+  const showPicture = (index: number) => {
+    setPhotoIndex(index)
+    setIsOpen(true)
+  }
+
+  return (
+    <>
+      <div className={styles.gallery}>
+        {pictures.map((picture: string, index: number) => (
+          <div
+            key={picture}
+            className={styles.thumbnailContainer}
+            style={{ position: 'relative', margin: '1px' }}
+            onClick={() => { showPicture(index) }}
+          >
+            <Image
+              className={styles.thumbnail}
+              src={picture}
+              layout="fill"
+              objectFit="cover"
+              sizes="400px"
+              alt=""
+            />
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-)
+
+      {isOpen && (
+        <Lightbox
+          mainSrc={pictures[photoIndex]}
+          nextSrc={pictures[(photoIndex + 1) % pictures.length]}
+          prevSrc={pictures[(photoIndex + pictures.length - 1) % pictures.length]}
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() =>
+            setPhotoIndex((photoIndex + pictures.length - 1) % pictures.length)
+          }
+          onMoveNextRequest={() =>
+            setPhotoIndex((photoIndex + 1) % pictures.length)
+          }
+        />
+      )}
+    </>
+  )
+}
 
 export default Gallery;
