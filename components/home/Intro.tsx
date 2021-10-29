534990d9c0cb91716fa9d527e01267602f0e009e
Michaël Perrin <michael.perrin84@gmail.com>
2021-10-29 12:19:17 +0200

    UI fixes

--- components/home/Intro.tsx
+++ components/home/Intro.tsx
@@ -4,6 +4,17 @@ const Intro = () => (
   <section className="intro">
     <div className="container">
       <div className="row align-items-center">
+        <div className="col-md-8 ml-md-auto">
+          <div className="content">
+            <h2>Randonnées en Ile-de-France</h2>
+
+            <p>RandoNavigo vous propose de nombreuses <strong>randonnées</strong> en Ile-de-France, toujours <strong>accessibles en transport en commun</strong> !</p>
+            <p>Il est en effet possible de s’évader en pleine nature près de chez soi et rapidement grâce aux transports en commun en Ile-de-France. Votre carte Navigo est désormais complètement dézonée, alors <strong>quittez la grisaille parisienne le temps d’une journée</strong> et profitez-en !</p>
+            <p>Vous serez étonné du nombre de randonnées possibles partout en Ile-de-France facilement accessibles <strong>sans voiture</strong>.</p>
+            <p>Je vous en propose quelques unes sur ce site web qui s’enrichira au fur et à mesure de nouvelles découvertes.</p>
+          </div>
+        </div>
+
         <div className="col-md-3">
           <aside className="gpx-info">
             <h3>Traces GPX</h3>
@@ -17,17 +28,6 @@ const Intro = () => (
               <a className="btn btn-info">En savoir plus</a>
             </Link>
           </aside>
-        </div>
-
-        <div className="col-md-8 ml-md-auto">
-          <div className="content">
-            <h2>Randonnées en Ile-de-France</h2>
-
-            <p>RandoNavigo vous propose de nombreuses <strong>randonnées</strong> en Ile-de-France, toujours <strong>accessibles en transport en commun</strong> !</p>
-            <p>Il est en effet possible de s’évader en pleine nature près de chez soi et rapidement grâce aux transports en commun en Ile-de-France. Votre carte Navigo est désormais complètement dézonée, alors <strong>quittez la grisaille parisienne le temps d’une journée</strong> et profitez-en !</p>
-            <p>Vous serez étonné du nombre de randonnées possibles partout en Ile-de-France facilement accessibles <strong>sans voiture</strong>.</p>
-            <p>Je vous en propose quelques unes sur ce site web qui s’enrichira au fur et à mesure de nouvelles découvertes.</p>
-          </div>
         </div>
       </div>
     </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="fr">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
