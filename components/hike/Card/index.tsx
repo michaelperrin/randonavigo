8b6dddaf8cc7ea3166e549c6462aa6c9cffe2b0d
Michaël Perrin <michael.perrin84@gmail.com>
2021-12-01 15:16:46 +0100

    Card round 2

--- components/hike/Card/index.tsx
+++ components/hike/Card/index.tsx
@@ -41,14 +41,15 @@ const HikeCard = ({ hike }: HikeCardProps) => (
 
 
 
-    <div className="flex flex-col flex-1 pt-4">
+    <div className="flex flex-col flex-1 pt-2">
       <div className="flex-grow pb-4 px-4">
         {hike.categories.length >= 0 && (
-          <div className="uppercase text-sm text-gray-500 leading-none">
+          <div className="uppercase text-sm text-gray-500">
             {/* Only display first category */}
             {hike.categories[0]}
           </div>
         )}
+
         <h2 className="font-sans-serif font-bold text-md mb-3">
           <Link href={getHikeUrl(hike)}>
             <a>
@@ -62,94 +63,25 @@ const HikeCard = ({ hike }: HikeCardProps) => (
         </div>
       </div>
 
-      <div className="relative">
-        {/* <div className="absolute inset-0">
+      <div className="flex px-4">
+        {/* <div className="flex pt-4 pb-5 px-4 backdrop-filter backdrop-blur-xl" style={{ backgroundColor: 'rgba(255, 255, 255, 0.85)' }}> */}
+        <div className="flex-grow text-sm pr-2">
+          <TransportPoint line={hike.starting_point.line} station={hike.starting_point.station} />
+        </div>
+        <div className="flex flex-shrink-0 items-center">
           <Image
-            src={getHikePicturePath(hike, hike.main_picture)}
-            layout="fill"
-            objectFit="cover"
-            sizes="350px"
-            quality={40}
-            className="rounded-t-md object-fill"
-            objectPosition="center bottom"
-            alt=""
+            src="/images/hike-icon.svg"
+            alt="Distance de marche"
+            className="hike-icon"
+            width={16}
+            height={16}
           />
-        </div> */}
-
-        <div className="flex px-4">
-          {/* <div className="flex pt-4 pb-5 px-4 backdrop-filter backdrop-blur-xl" style={{ backgroundColor: 'rgba(255, 255, 255, 0.85)' }}> */}
-          <div className="flex-grow text-sm pr-2">
-            <TransportPoint line={hike.starting_point.line} station={hike.starting_point.station} />
-          </div>
-          <div className="flex flex-shrink-0 items-center">
-            <Image
-              src="/images/hike-icon.svg"
-              alt="Distance de marche"
-              className="hike-icon"
-              width={16}
-              height={16}
-            />
-            <div>
-              {hike.distance}
-              km
-            </div>
+          <div>
+            {hike.distance}
+            km
           </div>
         </div>
       </div>
-
-      {/* <div className="rounded-b-md relative">
-        <div className="flex pt-4 pb-5 px-4" style={{ background: 'linear-gradient(151deg, hsl(240deg 8% 95%), hsl(30deg 6% 94%) 60%, hsl(30deg 16% 99%))' }}>
-          <div className="flex-grow text-sm">
-            <TransportPoint line={hike.starting_point.line} station={hike.starting_point.station} />
-          </div>
-          <div className="flex flex-shrink-0 items-center">
-            <Image
-              src="/images/hike-icon.svg"
-              alt="Distance de marche"
-              className="hike-icon"
-              width={16}
-              height={16}
-            />
-            <div>
-              {hike.distance}
-              km
-            </div>
-          </div>
-        </div>
-      </div> */}
-      {/* <div className="rounded-b-md relative">
-        <div className="absolute inset-0">
-          <Image
-            src={getHikePicturePath(hike, hike.main_picture)}
-            layout="fill"
-            objectFit="cover"
-            sizes="350px"
-            quality={40}
-            className="rounded-t-md object-fill"
-            objectPosition="center bottom"
-            alt=""
-          />
-        </div>
-
-        <div className="flex pt-4 pb-5 px-4 backdrop-filter backdrop-blur-none" style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)'}}>
-          <div className="flex-grow text-sm">
-            <TransportPoint line={hike.starting_point.line} station={hike.starting_point.station} />
-          </div>
-          <div className="flex flex-shrink-0 items-center">
-            <Image
-              src="/images/hike-icon.svg"
-              alt="Distance de marche"
-              className="hike-icon"
-              width={16}
-              height={16}
-            />
-            <div>
-              {hike.distance}
-              km
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   </article>
 )
