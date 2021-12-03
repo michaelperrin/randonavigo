17f6ee0749fd2ee4bca72c31cb351381572d9b0c
Michaël Perrin <michael.perrin84@gmail.com>
2021-12-03 19:09:25 +0100

    Simpler card design

--- components/hike/Card/index.tsx
+++ components/hike/Card/index.tsx
@@ -10,9 +10,9 @@ type HikeCardProps = {
 }
 
 const HikeCard = ({ hike }: HikeCardProps) => (
-  <article className="flex flex-col hike-card shadow-lg mb-4 bg-white">
+  <article className="flex flex-col hike-card mb-3 bg-white">
     <Link href={getHikeUrl(hike)}>
-      <a className="h-56 flex-none relative">
+      <a className="h-64 md:h-48 xl:h-64 flex-none relative">
         <div>
           {hike.favorite && (
             <div className="absolute top-0 right-0 z-10">
@@ -41,34 +41,36 @@ const HikeCard = ({ hike }: HikeCardProps) => (
 
 
 
-    <div className="flex flex-col flex-1 py-4">
-      <div className="flex-grow pb-4 px-4">
-        <div className="my-3">
+    <div className="flex flex-col flex-1">
+      <div className="px-0">
+        <div className="my-2">
           {hike.categories.length >= 0 && (
-            <div className="uppercase text-xs text-gray-500 text-center leading-none mb-1">
+            <div className="text-xs text-black text-center leading-none mt-5 mb-1 font-bold">
               {/* Only display first category */}
               {hike.categories[0]}
             </div>
           )}
-          <h2 className="font-sans-serif text-lg font-semibold text-center mx-8">
+
+          <h2 className="font-serif font-light text-xl text-center mx-8 mt-2 mb-4">
             <Link href={getHikeUrl(hike)}>
               <a>
-                { hike.title }
+                {hike.title}
               </a>
             </Link>
           </h2>
         </div>
 
-        <div className="text-gray-600 text-justify mb-4" style={{ fontSize: '15px', fontFamily: 'Lato' }}>
+        <div className="text-center text-gray-600 mb-2 font-light leading-relaxed" style={{ fontSize: '14.4px', fontFamily: 'Lato' }}>
           { hike.summary }
         </div>
       </div>
 
-      <div className="flex px-4">
-        <div className="flex-grow text-sm pr-2">
-          <TransportPoint line={hike.starting_point.line} station={hike.starting_point.station} />
+      <div className="flex flex-col justify-center">
+        <div className="text-sm flex items-center justify-center font-sans-serif mb-2 mt-2">
+          <TransportPoint line={hike.starting_point.line} station={hike.starting_point.station} iconSize={20} />
         </div>
-        <div className="flex flex-shrink-0 items-center">
+
+        <div className="flex items-center justify-center font-sans-serif text-sm font-medium">
           <Image
             src="/images/hike-icon.svg"
             alt="Distance de marche"
@@ -82,6 +84,10 @@ const HikeCard = ({ hike }: HikeCardProps) => (
           </div>
         </div>
       </div>
+
+      {/* <div className="mt-auto flex justify-center">
+        <div className="inline-block text-xs px-4 py-2 bg-gray-600 text-white tracking-wider">VIEW</div>
+      </div> */}
     </div>
   </article>
 )
--- components/hike/List.tsx
+++ components/hike/List.tsx
@@ -6,7 +6,7 @@ type ListProps = {
 }
 
 const List = ({ hikes }: ListProps) => (
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12 xl:gap-16">
     {hikes.map(hike => (
       <HikeCard hike={hike} key={hike.slug} />
     ))}
