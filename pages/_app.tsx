6df6fac8f498c68033c106dd5d1dd94550ca8171
Michaël Perrin <michael.perrin84@gmail.com>
2021-12-15 17:40:10 +0100

    Remove old styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from 'next/app'
 import Script from 'next/script'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-// import '../styles/global.scss'zff
 import 'tailwindcss/tailwind.css'
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.locatecontrol/dist/L.Control.Locate.css';
