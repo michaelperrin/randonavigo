72e51f73dfa258a3d4f517b223efecd8dd6fa669
Michaël Perrin <contact@michaelperrin.fr>
2022-01-25 09:27:27 +0100

    Block GPX files from being crawled

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,11 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.randonavigo.fr',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', 'disallow': '/*.gpx$/' },
+      { userAgent: '*', allow: '/' },
+    ],
+  },
   changefreq: 'monthly',
 }
