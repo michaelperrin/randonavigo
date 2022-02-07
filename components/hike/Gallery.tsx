145ab4cd523aa523749f4825edcca01327343870
Michaël Perrin <michael.perrin84@gmail.com>
2022-02-07 17:20:01 +0100

    Use zoom cursor over gallery images

--- components/hike/Gallery.tsx
+++ components/hike/Gallery.tsx
@@ -1,34 +1,41 @@
-import Image from 'next/image'
-import { useState } from 'react'
-import Lightbox from 'react-image-lightbox'
-import getHikePicturePath from '../../lib/getHikePicturePath'
-import { Hike } from '../../lib/types'
-import 'react-image-lightbox/style.css';
+import Image from "next/image";
+import { useState } from "react";
+import Lightbox from "react-image-lightbox";
+import getHikePicturePath from "../../lib/getHikePicturePath";
+import { Hike } from "../../lib/types";
+import "react-image-lightbox/style.css";
 
 type GalleryProps = {
-  hike: Hike,
-}
+  hike: Hike;
+};
 
 const Gallery = ({ hike }: GalleryProps) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [photoIndex, setPhotoIndex] = useState(0)
+  const [isOpen, setIsOpen] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
 
-  const pictures = hike.pictures.map((picture: string) => getHikePicturePath(hike, picture))
+  const pictures = hike.pictures.map((picture: string) =>
+    getHikePicturePath(hike, picture)
+  );
 
   const showPicture = (index: number) => {
-    setPhotoIndex(index)
-    setIsOpen(true)
-  }
+    setPhotoIndex(index);
+    setIsOpen(true);
+  };
 
   return (
     <>
-      <div className="grid gap-1" style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))' }}>
+      <div
+        className="grid gap-1"
+        style={{ gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))" }}
+      >
         {pictures.map((picture: string, index: number) => (
           <div
             key={picture}
-            className="aspect-w-1 aspect-h-1"
-            style={{ position: 'relative', margin: '1px' }}
-            onClick={() => { showPicture(index) }}
+            className="aspect-w-1 aspect-h-1 cursor-zoom-in"
+            style={{ position: "relative", margin: "1px" }}
+            onClick={() => {
+              showPicture(index);
+            }}
           >
             <Image
               src={picture}
@@ -46,7 +53,9 @@ const Gallery = ({ hike }: GalleryProps) => {
         <Lightbox
           mainSrc={pictures[photoIndex]}
           nextSrc={pictures[(photoIndex + 1) % pictures.length]}
-          prevSrc={pictures[(photoIndex + pictures.length - 1) % pictures.length]}
+          prevSrc={
+            pictures[(photoIndex + pictures.length - 1) % pictures.length]
+          }
           onCloseRequest={() => setIsOpen(false)}
           onMovePrevRequest={() =>
             setPhotoIndex((photoIndex + pictures.length - 1) % pictures.length)
@@ -57,7 +66,7 @@ const Gallery = ({ hike }: GalleryProps) => {
         />
       )}
     </>
-  )
-}
+  );
+};
 
 export default Gallery;
