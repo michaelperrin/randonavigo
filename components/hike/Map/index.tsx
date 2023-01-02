8203d0e23e53fcd40543cb4906c28ac47fb5c509
Michaël Perrin <michael.perrin84@gmail.com>
2023-01-02 16:05:25 +0100

    Disable gesture handling when map is in full screen mode
    This improves UX on mobile.

--- components/hike/Map/index.tsx
+++ components/hike/Map/index.tsx
@@ -3,29 +3,38 @@ import "leaflet-gesture-handling/dist/leaflet-gesture-handling.css";
 import withLeafletGestureHandling from "../../map/withLeafletGestureHandling";
 import MapContainer from "../../map/MapContainer";
 import getHikeGpxPath from "@/lib/getHikeGpxPath";
+import withFullScreenControl from "@/components/map/withFullScreenControl";
 
 type MapProps = {
   hike: Hike;
 };
 
-const MapContainerWithGestureHandling =
-  withLeafletGestureHandling(MapContainer);
+const compose = (...fns: Function[]) =>
+  fns.reduceRight(
+    (prevFn, nextFn) =>
+      (...args: any[]) =>
+        nextFn(prevFn(...args)),
+    (value: any) => value
+  );
+
+const ComposedMapContainer = compose(
+  withLeafletGestureHandling,
+  withFullScreenControl(true)
+)(MapContainer);
 
 const gpxFiles = (hike: Hike): string[] => [
   getHikeGpxPath(hike.slug, hike.publication_date, hike.gpx_file),
 ];
 
-const Map = ({ hike }: MapProps) => {
-  return (
-    <div>
-      <MapContainerWithGestureHandling
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-        gpxFiles={gpxFiles(hike)}
-      />
-    </div>
-  );
-};
+const Map = ({ hike }: MapProps) => (
+  <div>
+    <ComposedMapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      gpxFiles={gpxFiles(hike)}
+    />
+  </div>
+);
 
 export default Map;
