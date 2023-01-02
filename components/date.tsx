7c9e2b2eee65bc4688c55f680798fe24a9d90832
Michaël Perrin <michael.perrin84@gmail.com>
2023-01-02 16:11:09 +0100

    Code clean up and improved button design

--- components/date.tsx
+++ components/date.tsx
@@ -1,18 +1,22 @@
-import { parseISO, format } from 'date-fns'
-import { fr } from 'date-fns/locale'
+import { parseISO, format } from "date-fns";
+import { fr } from "date-fns/locale";
 
 type DateProps = {
-  dateString: string,
-  className?: string,
-}
+  dateString: string;
+  className?: string;
+};
 
 export default function Date({ dateString, className }: DateProps) {
-  const date = parseISO(dateString)
-  const extraAttributes: any = {}
+  const date = parseISO(dateString);
+  const extraAttributes: any = {};
 
-    if (className !== null) {
-      extraAttributes.className = className
-    }
+  if (className !== null) {
+    extraAttributes.className = className;
+  }
 
-  return <time dateTime={dateString} {...extraAttributes}>{format(date, 'd MMMM yyyy', { locale: fr })}</time>
+  return (
+    <time dateTime={dateString} {...extraAttributes}>
+      {format(date, "d MMMM yyyy", { locale: fr })}
+    </time>
+  );
 }
