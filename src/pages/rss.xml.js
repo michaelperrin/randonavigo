12ff042725b5527783048f801da0a52ae9e480d5
Michaël Perrin <michael.perrin84@gmail.com>
2025-06-27 14:50:38 +0200

    Rename "blog" to "hike" & prepare hikes properties card

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,16 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const hikes = await getCollection("hike");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: hikes.map((hike) => ({
+      ...hike.data,
+      link: `/hike/${hike.id}/`,
+    })),
+  });
 }
