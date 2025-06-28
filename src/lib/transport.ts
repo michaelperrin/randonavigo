1d801f4ac0f1bc0395922ccc569e542fc2e4a56e
Michaël Perrin <michael.perrin84@gmail.com>
2025-06-28 14:10:23 +0200

    Fix rendering of station that can be accessed by RER and Transilien (network enum: 0 and 2)

--- src/lib/transport.ts
+++ src/lib/transport.ts
@@ -1,6 +1,21 @@
-const RER_LINES = ['A', 'B', 'C', 'D', 'E']
-const TRAM_LINES = ['T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11', 'T12', 'T13']
-const TRANSILIEN_LINES = ['H', 'J', 'K', 'L', 'N', 'P', 'R', 'U']
+// TODO: use enums for lines
+const RER_LINES = ["A", "B", "C", "D", "E"];
+const TRAM_LINES = [
+  "T1",
+  "T2",
+  "T3",
+  "T4",
+  "T5",
+  "T6",
+  "T7",
+  "T8",
+  "T9",
+  "T10",
+  "T11",
+  "T12",
+  "T13",
+];
+const TRANSILIEN_LINES = ["H", "J", "K", "L", "N", "P", "R", "U"];
 
 export enum Network {
   RER,
@@ -8,23 +23,23 @@ export enum Network {
   Transilien,
 }
 
-export const getLineNetwork = (line: string): number => {
+export const getLineNetwork = (line: string): Network => {
   if (isRER(line)) {
-    return Network.RER
+    return Network.RER;
   }
 
   if (isTram(line)) {
-    return Network.Tram
+    return Network.Tram;
   }
 
   if (isTransilien(line)) {
-    return Network.Transilien
+    return Network.Transilien;
   }
 
-  throw new Error('Network for line could not be found')
-}
+  throw new Error("Network for line could not be found");
+};
 
-
-export const isRER = (line: string): boolean => RER_LINES.includes(line)
-export const isTransilien = (line: string): boolean => TRANSILIEN_LINES.includes(line)
-export const isTram = (line: string): boolean => TRAM_LINES.includes(line)
+export const isRER = (line: string): boolean => RER_LINES.includes(line);
+export const isTransilien = (line: string): boolean =>
+  TRANSILIEN_LINES.includes(line);
+export const isTram = (line: string): boolean => TRAM_LINES.includes(line);
