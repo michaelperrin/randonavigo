4e693cee6225babb04c38c0cdf7718d7b1d6817a
Michaël Perrin <michael.perrin84@gmail.com>
2025-07-04 16:12:39 +0200

    Add React support + install Leaflet

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.randonavigo.fr",
-  integrations: [
-    mdx({
-      remarkPlugins: [],
-      rehypePlugins: [],
-    }),
-    sitemap(),
-  ],
+  integrations: [mdx({
+    remarkPlugins: [],
+    rehypePlugins: [],
+  }), sitemap(), react()],
   vite: { plugins: [tailwindcss()] },
 });
