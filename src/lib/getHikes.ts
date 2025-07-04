b648837743d895b783153677c6da4d25c78b7339
Michaël Perrin <michael.perrin84@gmail.com>
2025-07-04 15:28:05 +0200

    Implement first hikes per line page (RER C)

--- src/lib/getHikes.ts
+++ src/lib/getHikes.ts
@@ -16,3 +16,31 @@ export async function getHikesSorted(): Promise<CollectionEntry<"hike">[]> {
     (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
   );
 }
+
+export async function getHikesForTransportLine(
+  line: string
+): Promise<CollectionEntry<"hike">[]> {
+  const hikes = await getHikes();
+
+  const hasLine = (hike: CollectionEntry<"hike">, line: string): boolean => {
+    if (
+      hike.data.starting_point.line === line ||
+      hike.data.starting_point.line.includes(line)
+    ) {
+      return true;
+    }
+
+    if (
+      hike.data.ending_point?.line === line ||
+      hike.data.ending_point?.line.includes(line)
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const hikesForLine = hikes.filter((hike) => hasLine(hike, line));
+
+  return hikesForLine;
+}
