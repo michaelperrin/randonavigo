b19a74530b58a891b453cc3b70084a7f887ae246
Michaël Perrin <michael.perrin84@gmail.com>
2025-07-28 20:38:06 +0200

    Add getHikeUrl utility and refactor HikeCard and RSS feed to use it

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,7 @@
 import rss from "@astrojs/rss";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
-import { getHikes } from "../lib/getHikes";
+import { SITE_TITLE, SITE_DESCRIPTION } from "~/consts";
+import { getHikes } from "~/lib/getHikes";
+import { getHikeUrl } from "~/lib/getHikeUrl";
 
 export async function GET(context) {
   const hikes = await getHikes();
@@ -9,14 +10,9 @@ export async function GET(context) {
     description: SITE_DESCRIPTION,
     site: context.site,
     items: hikes.map((hike) => {
-      const pubDate = hike.data.pubDate;
-      const year = pubDate.getFullYear().toString();
-      const month = (pubDate.getMonth() + 1).toString().padStart(2, '0');
-      const day = pubDate.getDate().toString().padStart(2, '0');
-
       return {
         ...hike.data,
-        link: `/${year}/${month}/${day}/${hike.id}/`,
+        link: getHikeUrl(hike),
       };
     }),
   });
