5d2244af04e432d3ed9c9c79c88cb9cdb035df13
Michaël Perrin <michael.perrin84@gmail.com>
2025-07-28 21:14:15 +0200

    Add favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "RandoNavigo";
+export const SITE_DESCRIPTION =
+  "Randonnées en Ile-de-France accessibles en transport en commun.";
