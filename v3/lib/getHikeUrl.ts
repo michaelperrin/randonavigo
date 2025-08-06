8930ff4a95164ba813551f9988b3870b1ad422c6
Michaël Perrin <michael.perrin84@gmail.com>
2025-08-06 17:33:06 +0200

    Remove old code

--- v3/lib/getHikeUrl.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from "dayjs";
-import { Hike } from "./types";
-
-const getHikeUrl = (hike: Hike) => {
-  return `/${dayjs(hike.publication_date).format("YYYY/MM/DD")}/${hike.slug}`;
-};
-
-export default getHikeUrl;
